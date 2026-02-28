adddec701cd41ac527f8c35e7cf396f22b28b616
Yukile394 <muhamedmuhet@gmail.com>
2026-02-28 07:00:08 +0300

    Update LoginX.java

--- src/main/java/com/loginx/LoginX.java
+++ src/main/java/com/loginx/LoginX.java
@@ -1,172 +1,136 @@
-package com.hitx;
+package com.loginx;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class HitX extends JavaPlugin implements Listener {
 
+    private File configFile;
     private FileConfiguration config;
-    private final HashMap<UUID, String> registered = new HashMap<>();
+    private final HashMap<UUID, String> logins = new HashMap<>();
     private final HashMap<UUID, Boolean> loggedIn = new HashMap<>();
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        this.config = getConfig();
-        getServer().getPluginManager().registerEvents(this, this);
+        configFile = new File(getDataFolder(), "config.yml");
+        config = YamlConfiguration.loadConfiguration(configFile);
 
-        getCommand("register").setExecutor(new RegisterCommand());
-        getCommand("login").setExecutor(new LoginCommand());
-        getCommand("logingoster").setExecutor(new LoginGosterCommand());
-        getCommand("incele").setExecutor(new InceleCommand());
+        Bukkit.getPluginManager().registerEvents(this, this);
+        this.getCommand("register").setExecutor(new RegisterCommand());
+        this.getCommand("login").setExecutor(new LoginCommand());
+        this.getCommand("logingoster").setExecutor(new LoginGosterCommand());
+
+        getLogger().info("HitX aktif edildi!");
     }
 
-    // Oyuncu Join
+    @Override
+    public void onDisable() {
+        getLogger().info("HitX kapatıldı!");
+    }
+
+    // Oyuncu join
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        if(!registered.containsKey(p.getUniqueId())){
-            loggedIn.put(p.getUniqueId(), false);
-            p.sendTitle(config.getString("title.register"), "", 10, 70, 20);
-            p.playSound(p.getLocation(), Sound.valueOf(config.getString("sounds.register_success")), 1, 1);
-        } else {
-            loggedIn.put(p.getUniqueId(), false);
-            p.sendTitle(config.getString("title.login"), "", 10, 70, 20);
-        }
+        loggedIn.put(p.getUniqueId(), false);
+
+        p.sendTitle(ChatColor.LIGHT_PURPLE + "Kayıt Ol",
+                ChatColor.translateAlternateColorCodes('&', "/register şifre şifre"),
+                10, 70, 20);
+        p.sendMessage(ChatColor.LIGHT_PURPLE + "Sunucuya giriş yaptın. Lütfen kaydol veya giriş yap!");
     }
 
-    // Chat kısıtlama
-    @EventHandler
-    public void onChat(AsyncPlayerChatEvent e) {
-        if(config.getBoolean("force_login") && !loggedIn.getOrDefault(e.getPlayer().getUniqueId(), false)) {
-            e.setCancelled(true);
-            e.getPlayer().sendMessage(ChatColor.RED + "Önce giriş yapmalısın!");
-        }
-    }
-
-    // Hareket kısıtlama
+    // Hareket engelleme login olmadan
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
-        if(config.getBoolean("force_login") && !loggedIn.getOrDefault(e.getPlayer().getUniqueId(), false)) {
-            e.setCancelled(true);
-        }
-    }
-
-    // Envanter kısıtlama
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        if(config.getBoolean("force_login") && !loggedIn.getOrDefault(((Player)e.getWhoClicked()).getUniqueId(), false)) {
+        Player p = e.getPlayer();
+        if (!loggedIn.getOrDefault(p.getUniqueId(), false)) {
             e.setCancelled(true);
         }
     }
 
     // /register komutu
-    public class RegisterCommand implements CommandExecutor {
+    private class RegisterCommand implements CommandExecutor {
         @Override
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if(!(sender instanceof Player p)) return false;
-
-            if(args.length != 2){
+            if (!(sender instanceof Player p)) return false;
+            if (args.length != 2) {
                 p.sendMessage(ChatColor.RED + "Kullanım: /register <şifre> <şifre>");
-                return true;
+                return false;
             }
-
-            if(!args[0].equals(args[1])){
-                p.sendMessage(ChatColor.RED + "Şifreler eşleşmiyor!");
-                return true;
+            String sifre1 = args[0];
+            String sifre2 = args[1];
+            if (!sifre1.equals(sifre2)) {
+                p.sendMessage(ChatColor.RED + "Şifreler uyuşmuyor!");
+                return false;
             }
-
-            registered.put(p.getUniqueId(), args[0]);
+            logins.put(p.getUniqueId(), sifre1);
             loggedIn.put(p.getUniqueId(), true);
-            p.sendTitle(config.getString("title.register_success"), "", 10, 70, 20);
-            p.playSound(p.getLocation(), Sound.valueOf(config.getString("sounds.register_success")), 1, 1);
+            p.sendTitle(ChatColor.LIGHT_PURPLE + "Başarıyla kayıt oldun!", "", 10, 70, 20);
+            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f);
             return true;
         }
     }
 
     // /login komutu
-    public class LoginCommand implements CommandExecutor {
+    private class LoginCommand implements CommandExecutor {
         @Override
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if(!(sender instanceof Player p)) return false;
-
-            if(args.length != 1){
+            if (!(sender instanceof Player p)) return false;
+            if (args.length != 1) {
                 p.sendMessage(ChatColor.RED + "Kullanım: /login <şifre>");
-                return true;
+                return false;
             }
-
-            String pw = registered.get(p.getUniqueId());
-            if(pw == null){
-                p.sendMessage(ChatColor.RED + "Önce kayıt olmalısın!");
-                return true;
+            String sifre = args[0];
+            String kayıtlı = logins.get(p.getUniqueId());
+            if (kayıtlı == null) {
+                p.sendMessage(ChatColor.RED + "Önce /register ile kaydol!");
+                return false;
             }
-
-            if(!pw.equals(args[0])){
+            if (!kayıtlı.equals(sifre)) {
                 p.sendMessage(ChatColor.RED + "Şifre yanlış!");
-                return true;
+                return false;
             }
-
             loggedIn.put(p.getUniqueId(), true);
-            p.sendTitle(config.getString("title.login_success"), "", 10, 70, 20);
-            p.playSound(p.getLocation(), Sound.valueOf(config.getString("sounds.login_success")), 1, 1);
+            p.sendTitle(ChatColor.GREEN + "Başarıyla giriş yaptın!", "", 10, 70, 20);
+            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f);
             return true;
         }
     }
 
-    // /logingoster komutu
-    public class LoginGosterCommand implements CommandExecutor {
+    // /logingoster komutu (yetkililere özel)
+    private class LoginGosterCommand implements CommandExecutor {
         @Override
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if(!sender.hasPermission("hitx.logingoster")){
+            if (!sender.hasPermission("hitx.view")) {
                 sender.sendMessage(ChatColor.RED + "Yetkin yok!");
-                return true;
+                return false;
             }
-
-            sender.sendMessage(ChatColor.LIGHT_PURPLE + "---- HitX Login List ----");
-            for(UUID uuid : registered.keySet()){
-                Player p = Bukkit.getPlayer(uuid);
-                String name = p != null ? p.getName() : "Offline";
-                sender.sendMessage(ChatColor.AQUA + name + " | " + ChatColor.LIGHT_PURPLE + registered.get(uuid));
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "=== LoginX Logları ===");
+            for (UUID id : logins.keySet()) {
+                Player p = Bukkit.getPlayer(id);
+                String name = (p != null) ? p.getName() : "Offline";
+                String sifre = logins.get(id);
+                sender.sendMessage(ChatColor.LIGHT_PURPLE + "Oyuncu: " + name + " | Şifre: " + sifre);
             }
-            return true;
-        }
-    }
-
-    // /incele komutu
-    public class InceleCommand implements CommandExecutor {
-        @Override
-        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if(!sender.hasPermission("hitx.incele")) return true;
-            if(args.length != 1){
-                sender.sendMessage(ChatColor.RED + "Kullanım: /incele <oyuncu>");
-                return true;
-            }
-
-            OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
-            sender.sendMessage(ChatColor.LIGHT_PURPLE + "--- Bilgi ---");
-            sender.sendMessage(ChatColor.AQUA + "İsim: " + ChatColor.LIGHT_PURPLE + target.getName());
-            sender.sendMessage(ChatColor.AQUA + "UUID: " + ChatColor.LIGHT_PURPLE + target.getUniqueId());
-            sender.sendMessage(ChatColor.AQUA + "Çevrimiçi: " + ChatColor.LIGHT_PURPLE + target.isOnline());
             return true;
         }
     }
